2049d8f747d30944aca8372325fc542841909cee
ibnul42 <ibnulashir42@gmail.com>
2021-06-03 20:57:49 +0600

    connected with api

--- js/index.js
+++ js/index.js
@@ -8,8 +8,41 @@ window.onload = () => {
 }
 
 const getPosts = () => {
+    fetch(API_URL, {
+        method: 'GET'
+    }).then((response) => {
+        return response.json();
+    }).then((data) => {
+        buildPosts(data);
+    })
 }
 
 const buildPosts = (blogPosts) => {
+    console.log(blogPosts);
+    let blogPostContent = "";
+    for(blogPost of blogPosts){
+        const blogDate = new Date(parseInt(blogPost.added_date)).toDateString();
+        const blogImage = `${API_BASE_URL}${blogPost.post_image}`
+        blogPostContent += `
+        <div class="post">
+            <div class="post-image" style="background-image: url(${blogImage})">
+            </div>
+            <div class="post-content">
+                <div class="post-date">
+                    ${blogDate}
+                </div>
+                <div class="post-title">
+                    <h4>${blogPost.title}</h4>
+                </div>
+                <div class="post-text">
+                ${blogPost.content}
+                </div>
+            </div>
+        </div>
+        `
+    }
+    document.querySelector('.blog-posts').innerHTML = blogPostContent;
 
+
+    
 }
