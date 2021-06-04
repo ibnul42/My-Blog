60facfe42a4d1e1f8d62538ecf494d131f5ac6dc
ibnul42 <ibnulashir42@gmail.com>
2021-06-04 09:05:46 +0600

    changed

--- js/index.js
+++ js/index.js
@@ -22,23 +22,26 @@ const buildPosts = (blogPosts) => {
     let blogPostContent = "";
     for(blogPost of blogPosts){
         const blogDate = new Date(parseInt(blogPost.added_date)).toDateString();
-        const blogImage = `${API_BASE_URL}${blogPost.post_image}`
+        const blogImage = `${API_BASE_URL}${blogPost.post_image}`;
+        const postLink = `/post.html?id=${blogPost.id}`;
         blogPostContent += `
-        <div class="post">
-            <div class="post-image" style="background-image: url(${blogImage})">
+        <a class="post-link" href="${postLink}">        
+            <div class="post">
+                <div class="post-image" style="background-image: url(${blogImage})">
+                </div>
+                <div class="post-content">
+                    <div class="post-date">
+                        ${blogDate}
+                    </div>
+                    <div class="post-title">
+                        <h4>${blogPost.title}</h4>
+                    </div>
+                    <div class="post-text">
+                    ${blogPost.content}
+                    </div>
+                </div>
             </div>
-            <div class="post-content">
-                <div class="post-date">
-                    ${blogDate}
-                </div>
-                <div class="post-title">
-                    <h4>${blogPost.title}</h4>
-                </div>
-                <div class="post-text">
-                ${blogPost.content}
-                </div>
-            </div>
-        </div>
+        </a>
         `
     }
     document.querySelector('.blog-posts').innerHTML = blogPostContent;
--- js/post.js
+++ js/post.js
@@ -8,14 +8,38 @@ const API_URL = "http://localhost:3000/api/posts/";
 const API_BASE_URL = "http://localhost:3000/";
 
 window.onload = () => {
-    getPost();
+    const postId = getPostIdParam();
+    getPost(postId);
 }
 
-const getPost = () => {
+const getPostIdParam = ()  => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    console.log(urlParams, queryString)
+    return urlParams.get('id');
+}
+
+const getPost = (postId) => {
     // CODE GOES HERE
+    const url = `${API_URL}${postId}`
+    fetch(url, {
+        method: 'GET'
+    }).then( (response) => {
+        return response.json();
+    }).then((data) => {
+        buildPost(data);
+    })
 }
 
 const buildPost = (data) => {
     // HINT: Convert the date number to a Date string 
+    const postDate = new Date(parseInt(data.added_date)).toDateString();
+    console.log(postDate);
+    const postImage = `${API_BASE_URL}${data.post_image}`;
+
+    document.querySelector('header').style.backgroundImage = `url(${postImage})`;
+    document.getElementById("individual-post-title").innerHTML = data.title;
+    document.getElementById("individual-post-date").innerHTML = postDate;
+    document.getElementById("individual-post-content").innerHTML = data.content; 
 }
 
